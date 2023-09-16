e12f5c0d0205b2529e4e151cbc1696168b8d1682
pabloleonardoo <luckyxd136@gmail.com>
2023-09-15 22:47:49 -0300

    update
    criei o a pagina de alterar , coloquei oque faltava no middleware para funcionar e criei a pagina de registrar e tambem baixei as dependencias do toastycontainer react

--- src/middleware.js
+++ src/middleware.js
@@ -6,6 +6,7 @@ export const middleware = (request) => {
 
     const token = request.cookies.get('token')?.value;
     const urlLogin = new URL('/', request.url);
+    const Users = new URL('./pages/dashboard' , request.url);
     const isTokenValidated = validateToken(token);
 
     if (!isTokenValidated || !token) {
