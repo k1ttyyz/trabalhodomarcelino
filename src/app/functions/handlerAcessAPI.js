3f17179bcfb3258edf0d61042123823b616c2a4e
pabloleonardoo <luckyxd136@gmail.com>
2023-09-15 21:47:09 -0300

    handleracessapi
    aqui é onde iremos definir os usuarios a serem autenticados e quais seus requisitos para serem autenticados

--- src/app/functions/handlerAcessAPI.js
+++ src/app/functions/handlerAcessAPI.js
@@ -1,10 +1,43 @@
 'use server'
 
-const getUserAuthenticated = (user) => {
-
+const Users = [{
+    nome:'pablo',
+    email:'pablo@gmail.com',
+    senha:'12345',
+    token:'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c'
+},{
+    nome:'maria',
+    email:'maria@gmail.com',
+    senha:'12345',
+    token:'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c'
+},{
+    nome:'maylla',
+    email:'maylla@gmail.com',
+    senha:'12345',
+    token:'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c'
+},{
+    nome:'lindo',
+    email:'lindo@gmail.com',
+    senha:'12345',
+    token:'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c'
 }
+//aqui serão os usuarios que seram registrados ou cadastrados no nosso formulário
+];
 
-const getUsers = () =>{
-        
+const getUserAuthenticated = async (userLogin) => {
+    let userAuth = {};//variavel para definir usuarios autenticados ou a serem registrados
+
+Users.map((user)=>{
+    if (user.email === userLogin.email & user.senha === userLogin.password){
+        userAuth = user
+    }
+}) 
+// mapeamento para definir os requisitos dos usuarios a serem preenchidos para registrar o usuario
+return userAuth;
 }
-export { getUsers, getUserAuthenticated };
+const getUsers = () => {
+    return Users;
+}
+export {getUsers, getUserAuthenticated};
+
+//aqui é aonde vai analisar o usuario autenticado pelo email , senha e também (opcional pelo nome)
