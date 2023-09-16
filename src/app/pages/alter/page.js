9c149e418049d24152ec9b4273f2eb574b108339
pabloleonardoo <luckyxd136@gmail.com>
2023-09-16 19:35:37 -0300

    update do trabalho
    aqui eu estilizei a pagina alter e também mudei de lugar a lista de usuarios do dashboard para que o suspense fique de uma cor diferente da lista , no alter eu fiz um formulário simples e tentei por a foto da lana mas n foi porque n combinou com a pagina

--- src/app/pages/alter/page.js
+++ src/app/pages/alter/page.js
@@ -2,6 +2,7 @@
 import { ToastContainer, toast } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
 
+
 const Formulariozinho = () => {
 
     const handlerLogin = async (e) => {
@@ -11,9 +12,33 @@ const Formulariozinho = () => {
 
     return(
       
-<div>
-    <form>
+
+    <div className="a">
+    <form onSubmit={handlerLogin}>
+    <h2 className="dd">FORMULÁRIO DE ALTERAR</h2>
+      <div className="containeer">
+   
+      <div className="b">
+      
+     
+       <br/>
+       <input placeholder="email" type="E-mail" className="innna"></input><br/>
+       <br/>
+       <input placeholder="name" type="nome" className="innne"></input><br/>
+       <br/>
+       <input placeholder="password" type="senha" className="inne"></input>
+       <br/>
+       <div  className="buu">
+       <button>Entrar</button>
+       </div>
+       <div className="bu">
+       <button ><a href="/pages/dashboard">Voltar</a></button>
+       </div>
+       </div>
+       </div>
+
 </form>
+
 <ToastContainer/>
 </div>
 
--- src/app/pages/dashboard/page.js
+++ src/app/pages/dashboard/page.js
@@ -7,14 +7,18 @@ export default async function Dashboard() {
     const users = getUsers()
     return (
         <div>
-           <Suspense fallback={<p>Carregando a pagina!!<img src="https://i.pinimg.com/originals/98/81/5f/98815f30af15d94ab3dd1af44ef8e6a9.gif"></img></p>}>
-            <Listarsim users ={users}/>
+        <div className="jj">
+           <Suspense fallback={<p><img src="https://i.pinimg.com/originals/98/81/5f/98815f30af15d94ab3dd1af44ef8e6a9.gif" className="hhl"></img><p className="hh">Carregando a pagina!!</p></p>}>
+            
+           
             <div className="bg-gray-200 w-100 italic">
-            <button><a href="/pages/alter">Alterar</a></button><br/>
+            <Listarsim users ={users}/>
+                
+            <button><a href="/pages/alter">Alterar</a></button>
             <button><a href="/pages/registraar">Registrar</a></button>
             </div>
            </Suspense>
-            
+           </div>
         </div>
     );
 };
