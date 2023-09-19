308cb6863daa926a167af7234c88349190ba0b70
pabloleonardoo <luckyxd136@gmail.com>
2023-09-19 01:27:38 -0300

    corrigindo
    tentativa de corrigir para voltar pra login

--- src/app/middleware.js
+++ src/app/middleware.js
@@ -6,29 +6,24 @@ export const middleware = (request) => {
 
     const token = request.cookies.get('token')?.value;
     const urlLogin = new URL('/', request.url);
-    const urlDashh = new URL('/pages/dashboard' , request.url);
+    const urldashboard = new URL('/pages/dashboard' , request.url)
     const isTokenValidated = validateToken(token);
 
     if (!isTokenValidated || !token) {
-        if (request.nextUrl.pathname === '/pages/dashboard' || request.nextUrl.pathname === '/pages/registraar' || request.nextUrl.pathname === '/pages/alter') {
-        {
+        if (request.nextUrl.pathname === '/pages/dashboard'|| 
+        request.nextUrl.pathname === '/pages/alter' || request.nextUrl.pathname === '/pages/registraar'
+        )  {
             return NextResponse.redirect(urlLogin);
         }
     }
-    }
-
-    if (!isTokenValidated || !token) {
-        if (request.nextUrl.pathname === '/') 
-        {
-            return NextResponse.redirect(urlDashh);
+    if (isTokenValidated) {
+        if (request.nextUrl.pathname === '/') {
+            return NextResponse.redirect(urldashboard);
         }
     }
-
-
     NextResponse.next();
 };
 export const config = {
-    matcher: ['/', '/pages/dashboard', '/pages/alter', '/pages/registraar'] 
+    matcher: ['/', '/pages/dashboard', '/pages/alter', '/pages/registraar', ]
 };
-
 
