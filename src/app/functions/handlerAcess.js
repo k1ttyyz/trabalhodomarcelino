97b35857e24470fb3d454482e6979e920bb5bcf1
pabloleonardoo <luckyxd136@gmail.com>
2023-09-18 21:53:21 -0300

    handleraccess explicação
    o handler acess é uma forma de ver o token do usuario e validar a entrada dele no sistema para proxima pagina que seria o dashboard

--- src/app/functions/handlerAcess.js
+++ src/app/functions/handlerAcess.js
@@ -11,10 +11,13 @@ const handlerAcessUser = async (user) => {
     if (isTokenValidate) {
         Cookies.set('token', userAuth.token, { expires: 1 });
         if (typeof window !== 'undefined'){
-            localStorage.setItem('nome',userAuth.nome)
+            localStorage.setItem('nome',userAuth.nome),
+            localStorage.setItem('email',userAuth.email),
+            localStorage.setItem('password',userAuth.senha)
         }
         return userAuth;
     }
 }
 export default handlerAcessUser;
 
+//aqui basicamente ira pegar o token e os requerimentos da pagina de login e ira fornecedor pro localstorage mostrando oque voce colocou ao preencher o formulario
