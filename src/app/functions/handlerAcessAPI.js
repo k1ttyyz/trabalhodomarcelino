3eb26798ed4ce2a9a3ab510c5ea301f257c1c82e
k1ttyyz <luckyxd136@gmail.com>
2023-10-17 16:03:30 -0400

    pagina do handleracess novo

--- src/app/functions/handlerAcessAPI.js
+++ src/app/functions/handlerAcessAPI.js
@@ -1,43 +1,19 @@
 'use server'
+const url ="https://aula-17-10-main.vercel.app/";
 
-const Users = [{
-    nome:'pablo',
-    email:'pablo@gmail.com',
-    senha:'12345',
-    token:'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c'
-},{
-    nome:'maria',
-    email:'maria@gmail.com',
-    senha:'12345',
-    token:'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c'
-},{
-    nome:'maylla',
-    email:'maylla@gmail.com',
-    senha:'12345',
-    token:'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c'
-},{
-    nome:'lindo',
-    email:'lindo@gmail.com',
-    senha:'12345',
-    token:'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c'
+const getUserAuthenticated = async (user) => {
+const responseOffApi = await fetch (url + "/user/authenticate",
+{
+    method:"POST",
+    headers: {"Content-Type":"application/json"},
+    body: JSON.stringify(user)
 }
-//aqui serão os usuarios que seram registrados ou cadastrados no nosso formulário
-];
-
-const getUserAuthenticated = (user) => {
-    let userAuth = {};//variavel para definir usuarios autenticados
-
-Users.map((gato)=>{
-    if (user.email === gato.email & gato.senha === user.password & user.name === gato.nome){
-        userAuth = gato
-    }
-}) 
-// mapeamento para definir os requisitos dos usuarios a serem preenchidos para registrar o usuario
+);
+const userAuth = await responseOffApi.json();
 return userAuth;
 }
-const getUsers = () => {
-    return Users;
-}
-export {getUsers, getUserAuthenticated};
+const getUsers = async () => {
 
-//aqui é aonde vai analisar o usuario autenticado pelo email , senha e também (opcional pelo nome)
+}
+
+export { getUsers, getUserAuthenticated};
