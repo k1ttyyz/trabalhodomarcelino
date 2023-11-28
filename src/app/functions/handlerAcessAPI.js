4414be798f33776f1d7675fbe66ed31f9a5e1002
k1ttyyz <luckyxd136@gmail.com>
2023-11-28 16:10:57 -0400

    tentando arrumar register

--- src/app/functions/handlerAcessAPI.js
+++ src/app/functions/handlerAcessAPI.js
@@ -1,9 +1,9 @@
 'use server'
-const url ="https://aula-17-10-main-gilt.vercel.app";
+const url ="https://aula-17-10-main-kwjg.vercel.app";
 
 const getUserAuthenticated = async (user) => {
 
-   const responseOfApi = await fetch(url + "/user/authenticated",
+   const responseOffApi = await fetch(url + "/user/authenticated",
     {
         method: "POST",
         cache:"no-cache",
@@ -18,20 +18,24 @@ return userAuth;
 
 
 const postUser = async (user) => {
-try{
-    const responseOfApi = await fetch(url + "/user",{
-     
+
+    const responseOffApi = await fetch(url + "/user",
+     {
          method: "POST",
          cache:"no-cache",
-         headers: {"Content-Type" : "Application/json"}, 
+         headers: {"Content-Type" : "application/json"}, 
          body: JSON.stringify(user)
-     });
- const userSavee = await responseOfApi.json();
+     }
+     )
+ const userSavee = await responseOffApi.json();
+ console.log(userAuth)
  return userSavee;
- }catch{
-    return null;
  }
-}
+
+
+
+
+
 
 const getUsers = async (user) => {
 
--- src/app/functions/validateToken.js
+++ src/app/functions/validateToken.js
@@ -1,13 +1,18 @@
 import { jwtVerify } from "jose";
-
+import { decode } from "jsonwebtoken";
 
 const validateToken = async(token) => {
     const secret = "eyJhbGciOiJIUzI1NiJ9.eyJSb2xlIjoiQWRtaW4iLCJJc3N1ZXIiOiJJc3N1ZXIiLCJVc2VybmFtZSI6IkphdmFJblVzZSIsImV4cCI6MTY5NTY5MTU0MiwiaWF0IjoxNjk1NjkxNTQyfQ.EG-A9n08TjSSm2affCX5Bl3yRpDbIytMIO5j2iMalLA"
 try{
-    const isTokenValidate = await jwtVerify(token, new TextEncader().encode(secret));
+    /*const isTokenValidate = await jwtVerify(token, new TextEncader().encode(secret));
     console.log(isTokenValidate)
     if(isTokenValidate){
         return true 
+    }*/
+
+    const isTokenValidate = await decode(token);
+    if (isTokenValidate) {
+        return true
     }
 } catch{
     return false;
